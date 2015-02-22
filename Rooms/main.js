d12cbe9d25e15d53b172d3084a4e06fea57ac339
Evochio <arghjn@hotmail.com>
2015-02-22 19:21:51 +0100

    Adding files

---
+++ Rooms/main.js
@@ -0,0 +1,51 @@
+lib = {}
+
+lib.load = function (key) {
+  var value = localStorage.getItem(key);
+  try {
+    value = JSON.parse(value);
+    return value;
+  }
+  catch (SyntaxError) {
+    return null;
+  }
+}
+
+lib.save = function (key, value) {
+  localStorage.setItem(key, JSON.stringify(value))
+};
+
+lib.goTo = function (href) {
+  var parts = href.split('/');
+  var lastPart = parts[parts.length-1];
+  lib.save('state-'+lastPart, state);
+  document.location.href = href;
+}
+
+
+// EDIT THIS //////////////
+// initialState is how you want the state to be at the beginning of the game.
+var initialState = {
+  crewMorale: 6,
+  inventory: []
+}
+///////////////////////////
+
+var parts = document.location.href.split('/');
+var lastPart = parts[parts.length-1];
+state = lib.load('state-'+lastPart) || initialState;
+
+
+$(document).ready(function() {
+  // EDIT THIS ///////////////
+  // Code you want to run at the beginning of every page load goes here.
+  // For example, code that tests the state to see if morale is low enough to redirect you to an ending.
+  
+  if (state.crewMorale < -4) {
+    document.location.href = '/terrible-end.html';
+  }
+  
+  console.log('Current state:', JSON.stringify(state, null, '\t'))
+  
+  ////////////////////////////
+});
